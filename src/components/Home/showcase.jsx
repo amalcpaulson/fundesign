38792360e2279518d53e900ee5cef7e212f73e25
Amal C P <acp070146@gmail.com>
2023-07-06 13:54:56 +0530

    slider created

--- src/components/Home/showcase.jsx
+++ src/components/Home/showcase.jsx
@@ -3,10 +3,42 @@ import "../../pages/Home/home.css";
 import mac from "../../assets/Home/showcase/Mac.png";
 import vectorleft from "../../assets/Home/showcase/Vector-left.png";
 import vectorright from "../../assets/Home/showcase/Vector-right.png";
-import "./slider.css";
-import './slider'
+
 const showcase = () => {
-    
+    const slider = document.querySelector(".slider");
+    const containerWidth = document.querySelector(".container").offsetWidth;
+    let scrollPosition = 0;
+
+    document.addEventListener("DOMContentLoaded", () => {
+      updateSliderPosition();
+    });
+
+    function updateSliderPosition() {
+      slider.style.transform = `translateX(${-scrollPosition}px)`;
+    }
+
+    function scrollToNextContainer() {
+      const numContainers = document.querySelectorAll(".container").length;
+      if (scrollPosition < containerWidth * (numContainers - 1)) {
+        scrollPosition += containerWidth;
+        updateSliderPosition();
+      }
+    }
+
+    function scrollToPreviousContainer() {
+      if (scrollPosition > 0) {
+        scrollPosition -= containerWidth;
+        updateSliderPosition();
+      }
+    }
+
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "ArrowRight") {
+        scrollToNextContainer();
+      } else if (event.key === "ArrowLeft") {
+        scrollToPreviousContainer();
+      }
+    });
   return (
     <div className="showcase-home-wrapper">
       <h1>Making us stand out!</h1>
@@ -16,32 +48,25 @@ const showcase = () => {
         </div>
         <div className="showcase-home-content-mid">
           <img src={mac} alt="" />
-          <div>
-            <div class="slider">
-              <div class="slider__slides">
-                <div class="slide s--active">
-                  <div class="slide__inner"></div>
-                </div>
-                <div class="slide">
-                  <div class="slide__inner"></div>
-                </div>
-                <div class="slide">
-                  <div class="slide__inner"></div>
-                </div>
-                <div class="slide">
-                  <div class="slide__inner"></div>
-                </div>
-                <div class="slide s--prev">
-                  <div class="slide__inner"></div>
-                </div>
+          <div className="slider-wrapper">
+            <div className="slider">
+              <div className="container">
+                <iframe
+                  src="https://codedesignweek.netlify.app/"
+                  frameborder="0"
+                ></iframe>
               </div>
-              <div class="slider__control">
-                <div class="slider__control-line"></div>
-                <div class="slider__control-line"></div>
+              <div className="container">
+                <iframe
+                  src="https://mulearn-cce.vercel.app/"
+                  frameborder="0"
+                ></iframe>
               </div>
-              <div class="slider__control slider__control--right m--right">
-                <div class="slider__control-line"></div>
-                <div class="slider__control-line"></div>
+              <div className="container">
+                <iframe
+                  src="https://amalcp-portfolio.netlify.app/"
+                  frameborder="0"
+                ></iframe>
               </div>
             </div>
           </div>
