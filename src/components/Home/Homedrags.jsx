dce892098095748223ff5fa2dce6812d0e9fa2e7
Amal C P <acp070146@gmail.com>
2023-07-07 12:21:43 +0530

    dragable contents

---
+++ src/components/Home/Homedrags.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "../../pages/Home/home.css";
+import Draggable, { DraggableCore } from "react-draggable";
+const Homedrag = () => {
+  console.log(window.innerWidth, window.innerHeight);
+  const [widthHeight, setWidthHeight] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  return (
+    <>
+      <div className="homedrag-wrapper-desk">
+        <Draggable defaultPosition={{ x: widthHeight.width - 100, y: 30 }}>
+          <h1>Hello</h1>
+        </Draggable>
+        <Draggable defaultPosition={{ x: widthHeight.width - 400, y: 30 }}>
+          <h1>Hello</h1>
+        </Draggable>
+        <Draggable defaultPosition={{ x: 400, y: 300 }}>
+          <div>Hello</div>
+        </Draggable>
+      </div>
+    </>
+  );
+};
+
+export default Homedrag;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./home.css";
 import Navbar from "../../components/navbar/Navbar";
 import Company from "../../components/Home/company";
-import Homedrag from "../../components/Home/homedrag";
+import Homedrag from "../../components/Home/Homedrags";
 import About from "../../components/Home/about";
 import Showcase from "../../components/Home/showcase";
 
