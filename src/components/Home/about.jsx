ae1d2d9a1ca0b6c05c29072852549187956f689b
Amal C P <acp070146@gmail.com>
2023-07-19 23:11:50 +0530

    earth image section added

--- src/components/Home/about.jsx
+++ src/components/Home/about.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "../../pages/Home/home.css";
-import arrow from "../../assets/Home/about/arrow.png";
+import earth from "../../assets/Home/about/loveearth.png";
 import brand from "../../assets/Home/about/brand.png";
 import graphic from "../../assets/Home/about/graphic.png";
 import uiux from "../../assets/Home/about/uiux.png";
@@ -9,64 +9,108 @@ import web from "../../assets/Home/about/web.png";
 
 const about = () => {
   return (
-    <div className="home-about-wrapper">
-      <h1>Empowering You!</h1>
-      <div className="home-about-content-wrapper">
-        <div className="home-about-content-individual">
-          <img src={graphic} alt="" />
-          <div>
-            <h3>Graphic Design</h3>
-            <p>
-              Graphic Stationaries, Poster Design, Magazine Design, Book Design
-              etc.
-            </p>
-          </div>
-          <a href="">
-            <p>Read More</p>
-            <img src={arrow} alt="" />
-          </a>
+    <div className="about-home-wrapper">
+      <div className="about-home-left-img">
+        <img src={earth} alt="" />
+      </div>
+      <div className="about-home-right-content">
+        <div>
+          <h1>WHY JOIN?</h1>
         </div>
-        <div className="home-about-content-individual">
-          <img src={uiux} alt="" />
-          <div>
-            <h3>UI/UX Design</h3>
+        <div className="about-home-right-content-div">
+          <div className="about-home-right-individual">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+              viewBox="0 0 48 43"
+              fill="none"
+            >
+              <path
+                d="M24.393 42.7843C22.8807 42.7843 21.6148 41.7626 21.367 40.3169C18.9509 27.4406 16.9928 25.6479 3.4717 24.2408C1.91284 24.0673 0.684413 22.8722 0.648117 21.4843C0.61132 20.0772 1.75723 18.9206 3.306 18.7086C16.6613 16.993 18.2813 15.4895 20.3814 2.63238C20.6172 1.20596 21.7877 0.184326 23.2789 0.184326C24.8123 0.184326 26.0572 1.20596 26.3469 2.65166C28.7206 15.5088 30.6787 17.3014 44.1788 18.7086C45.7801 18.9013 46.9865 20.0579 47.0238 21.4843C47.0601 22.8722 45.9562 24.0287 44.3655 24.2408C30.9901 25.9949 29.3907 27.4792 27.3324 40.3362C27.0552 41.7819 25.8633 42.7843 24.393 42.7843Z"
+                fill="url(#paint0_radial_438_296)"
+              />
+              <defs>
+                <radialGradient
+                  id="paint0_radial_438_296"
+                  cx="0"
+                  cy="0"
+                  r="1"
+                  gradientUnits="userSpaceOnUse"
+                  gradientTransform="translate(39.8339 35.6843) rotate(-154.937) scale(33.5206 31.9837)"
+                >
+                  <stop stop-color="#DABFFF" />
+                  <stop offset="1" stop-color="#874FFF" />
+                </radialGradient>
+              </defs>
+            </svg>
             <p>
-              UI / UX design, Concept Design, Mobile Application Design,
-              Interactive Designs
+              Until we can offer roles, we can{" "}
+              <span className="text-color-violet">share experience.</span>
             </p>
           </div>
-          <a href="">
-            <p>Read More</p>
-            <img src={arrow} alt="" />
-          </a>
-        </div>{" "}
-        <div className="home-about-content-individual">
-          <img src={web} alt="" />
-          <div>
-            <h3>Web Design</h3>
+          <div className="about-home-right-individual">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+              viewBox="0 0 48 43"
+              fill="none"
+            >
+              <path
+                d="M24.393 42.7843C22.8807 42.7843 21.6148 41.7626 21.367 40.3169C18.9509 27.4406 16.9928 25.6479 3.4717 24.2408C1.91284 24.0673 0.684413 22.8722 0.648117 21.4843C0.61132 20.0772 1.75723 18.9206 3.306 18.7086C16.6613 16.993 18.2813 15.4895 20.3814 2.63238C20.6172 1.20596 21.7877 0.184326 23.2789 0.184326C24.8123 0.184326 26.0572 1.20596 26.3469 2.65166C28.7206 15.5088 30.6787 17.3014 44.1788 18.7086C45.7801 18.9013 46.9865 20.0579 47.0238 21.4843C47.0601 22.8722 45.9562 24.0287 44.3655 24.2408C30.9901 25.9949 29.3907 27.4792 27.3324 40.3362C27.0552 41.7819 25.8633 42.7843 24.393 42.7843Z"
+                fill="url(#paint0_radial_438_296)"
+              />
+              <defs>
+                <radialGradient
+                  id="paint0_radial_438_296"
+                  cx="0"
+                  cy="0"
+                  r="1"
+                  gradientUnits="userSpaceOnUse"
+                  gradientTransform="translate(39.8339 35.6843) rotate(-154.937) scale(33.5206 31.9837)"
+                >
+                  <stop stop-color="#DABFFF" />
+                  <stop offset="1" stop-color="#874FFF" />
+                </radialGradient>
+              </defs>
+            </svg>
             <p>
-              Website Design, Icon Design, Web Application Design, E-Commerce
-              Website Design
+              Until we can hire more designers, we can{" "}
+              <span className="text-color-violet">help them develop.</span>
             </p>
           </div>
-          <a href="">
-            <p>Read More</p>
-            <img src={arrow} alt="" />
-          </a>
-        </div>{" "}
-        <div className="home-about-content-individual">
-          <img src={brand} alt="" />
-          <div>
-            <h3>Brand Identity</h3>
+          <div className="about-home-right-individual">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+              viewBox="0 0 48 43"
+              fill="none"
+            >
+              <path
+                d="M24.393 42.7843C22.8807 42.7843 21.6148 41.7626 21.367 40.3169C18.9509 27.4406 16.9928 25.6479 3.4717 24.2408C1.91284 24.0673 0.684413 22.8722 0.648117 21.4843C0.61132 20.0772 1.75723 18.9206 3.306 18.7086C16.6613 16.993 18.2813 15.4895 20.3814 2.63238C20.6172 1.20596 21.7877 0.184326 23.2789 0.184326C24.8123 0.184326 26.0572 1.20596 26.3469 2.65166C28.7206 15.5088 30.6787 17.3014 44.1788 18.7086C45.7801 18.9013 46.9865 20.0579 47.0238 21.4843C47.0601 22.8722 45.9562 24.0287 44.3655 24.2408C30.9901 25.9949 29.3907 27.4792 27.3324 40.3362C27.0552 41.7819 25.8633 42.7843 24.393 42.7843Z"
+                fill="url(#paint0_radial_438_296)"
+              />
+              <defs>
+                <radialGradient
+                  id="paint0_radial_438_296"
+                  cx="0"
+                  cy="0"
+                  r="1"
+                  gradientUnits="userSpaceOnUse"
+                  gradientTransform="translate(39.8339 35.6843) rotate(-154.937) scale(33.5206 31.9837)"
+                >
+                  <stop stop-color="#DABFFF" />
+                  <stop offset="1" stop-color="#874FFF" />
+                </radialGradient>
+              </defs>
+            </svg>
             <p>
-              Brand Identity, Logo Design, Visual Language Design, Brand
-              Graphics etc.
+              Until we can build careers, we can{" "}
+              <span className="text-color-violet">boost the community.</span>
             </p>
           </div>
-          <a href="">
-            <p>Read More</p>
-            <img src={arrow} alt="" />
-          </a>
         </div>
       </div>
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./home.css";
 import Navbar from "../../components/navbar/Navbar";
 import Landing from '../../components/Home/Homescreen'
+import About from "../../components/Home/about";
 import Projects from "../../components/Home/projects";
 import Software from "../../components/Home/software";
 import Team from "../../components/Home/Team";
@@ -19,6 +20,7 @@ const Home = () => {
       <Navbar />
       <div className="home-body">
         <Landing />
+        <About />
         <Projects />
         <Software />
         <Team />
